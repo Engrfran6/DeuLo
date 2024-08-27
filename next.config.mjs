0ad300153764dfa57a4b94a97569ecbca325c170
Francis <99579671+Engrfran6@users.noreply.github.com>
2024-08-27 11:00:34 +0100

    ignoring eslint during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {distDir: 'build'};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
